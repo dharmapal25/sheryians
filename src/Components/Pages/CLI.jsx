8ff0e0e1d757d66f44929165703d8cce21157758
dharmapal25 <dharmapalbharati2525@gmail.com>
2026-03-14 12:31:06 +0530

    terminal complete

--- src/Components/Pages/CLI.jsx
+++ src/Components/Pages/CLI.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
+import Terminal from 'react-console-emulator'
+import "./CLI.css"
+
 
 const CLI = () => {
+
+  let user = "Flash"
+
+  let space = "         "
+
+  const commands = {
+
+    hello: {
+      usage: 'hello',
+      fn: () => 'Hello, World!'
+    },
+
+    echo: {
+      usage: 'echo <text>',
+      fn: (...textInfo) => textInfo.join(" ")
+    },
+
+    whoiam: {
+      fn: () => user
+    },
+
+    admin: {
+      fn: () => `
+╔════════════════════╗
+ FLASH OS - ADMIN INFO
+╚════════════════════╝
+
+👨‍💻 Developer : Flash
+🎓 Role       : MERN Stack Developer
+⚙️ Skills     : React | Node.js | Express | MongoDB | System Design
+
+🚀 Current Projects
+• Web OS (browser based operating system)
+• WhatsApp Clone (MERN + Socket.IO)
+
+🧠 Interests
+• Backend Architecture
+• Real-time Applications
+• System Design
+
+🌐 GitHub
+• https://github.com/dharmapal25
+
+
+`
+    },
+
+    h: {
+      fn: () => 'hello - welcomeMessage \n echo - printt something \n whoiam - username \n admin - project info'
+    }
+
+
+
+  }
+
   return (
-    <div>
-        <h1>Command line interface</h1>
+    <div className="CLI-div">
+
+      <Terminal
+        commands={commands}
+        welcomeMessage={'Welcome to my terminal!\n h - Help'}
+        promptLabel={`${user}@terminal:~$`}
+      />
     </div>
+
   )
 }
 
