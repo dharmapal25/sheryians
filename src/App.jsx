c65d6e271b8a281969ebde134e0b7e828ff140f2
dharmapal25 <dharmapalbharati2525@gmail.com>
2026-03-15 15:38:58 +0530

    add a wallpaper feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./App.css"
 import Home from './Components/Home'
 
 const App = () => {
   return (
     <>
+
       <img src="https://ik.imagekit.io/cblndrocc/macos.jpg" className='appJsx-image' draggable="false" />
 
+          <img src={`../src/Images/Wallpapers/${localStorage.getItem("bgImage")}`} className='appJsx-image' draggable="false" />
 
       <Home />
     </>
--- src/Components/Pages/Setting.jsx
+++ src/Components/Pages/Setting.jsx
@@ -1,5 +1,19 @@
 import React, { useState } from 'react'
 import "./Setting.css"
+import Dark from "../../Images/themes/Dark.png";
+import light from "../../Images/themes/light.png";
+import DarkBlue from "../../Images/themes/DarkBlue.png";
+import Default from "../../Images/themes/Default.png";
+import image from "../../Images/Wallpapers/image.jpg";
+// import image1 from "../../Images/Wallpapers/image1.jpg";
+import image2 from "../../Images/Wallpapers/image2.jpg";
+import image3 from "../../Images/Wallpapers/image3.jpg";
+import image4 from "../../Images/Wallpapers/image4.jpg";
+import image5 from "../../Images/Wallpapers/image5.jpg";
+import image6 from "../../Images/Wallpapers/image6.jpg";
+import image7 from "../../Images/Wallpapers/image7.jpg";
+
+
 const Setting = () => {
 
     const [settingNavName, setSettingNavName] = useState("");
@@ -19,7 +33,6 @@ const Setting = () => {
             </div>
 
             <div className="Setting-body">
-                <span>Empty!</span>
                 {
                     (settingNavName == "Wallpapers") ? Wallpapers() :
                         (settingNavName == "Tab_style") ? Tab_style() : Themes()
@@ -39,17 +52,89 @@ export default Setting
 
 export const Themes = () => {
     return (
-        <div>
-            <h1>Themes!</h1>
-        </div>
+        <>
+            <p>Themes!</p>
+            <div className='Themes-div'>
+
+                <div className="default-div">
+                    <img className='theme-image' src={Default} />
+                    <button className='Themes-btn' >Set as Wallpaper</button>
+                </div>
+
+                <div className="dark-div">
+                    <img className='theme-image' src={Dark} />
+                    <button className='Themes-btn'>Set as Wallpaper</button>
+                </div>
+
+                <div className="light-div">
+                    <img className='theme-image' src={light} />
+                    <button className='Themes-btn'>Set as Wallpaper</button>
+                </div>
+
+                <div className="darkblue-div">
+                    <img className='theme-image' src={DarkBlue} />
+                    <button className='Themes-btn'>Set as Wallpaper</button>
+                </div>
+            </div>
+        </>
     )
 }
 
 export const Wallpapers = () => {
+    
+    // const [wallpaperImage, setWallpaperImage] = useState("");
+
+    function BGWallpapers(image) {
+        // console.log(image)
+        // setWallpaperImage(image)
+        localStorage.setItem("bgImage",image)
+        console.log(window.location.reload())
+    }
+
     return (
-        <div>
-            <h1>Wallpapers!</h1>
-        </div>
+        <>
+            <p>Wallpapers!</p>
+            <div className="wallpaper-images">
+
+                <div className="wallpaper-images-div">
+                    <img src={image} className='wallpaper-image' />
+                    <button className="Themes-btn" onClick={() => BGWallpapers("image.jpg")} >set as a wallpaper</button>
+                </div>
+
+                <div className="wallpaper-images-div">
+                    <img src={image2} className='wallpaper-image' />
+                    <button className="Themes-btn" onClick={() => BGWallpapers("image2.jpg")}>set as a wallpaper</button>
+                </div>
+
+                <div className="wallpaper-images-div">
+                    <img src={image3} className='wallpaper-image' />
+                    <button className="Themes-btn" onClick={() => BGWallpapers("image3.jpg")}>set as a wallpaper</button>
+                </div>
+
+                <div className="wallpaper-images-div">
+                    <img src={image4} className='wallpaper-image' />
+                    <button className="Themes-btn" onClick={() => BGWallpapers("image4.jpg")}>set as a wallpaper</button>
+                </div>
+
+                <div className="wallpaper-images-div">
+                    <img src={image5} className='wallpaper-image' />
+                    <button className="Themes-btn" onClick={() => BGWallpapers("image5.jpg")}>set as a wallpaper</button>
+                </div>
+
+                <div className="wallpaper-images-div">
+                    <img src={image6} className='wallpaper-image' />
+                    <button className="Themes-btn" onClick={() => BGWallpapers("image6.jpg")}>set as a wallpaper</button>
+                </div>
+
+                <div className="wallpaper-images-div">
+                    <img src={image7} className='wallpaper-image' />
+                    <button className="Themes-btn" onClick={() => BGWallpapers("image7.jpg")}>set as a wallpaper</button>
+                </div>
+
+
+            </div>
+
+        </>
     )
 }
 export const Tab_style = () => {
